53e0344a6ef2d04e2ec8e72abb0a41e93f8e43a7
brian@espressif.com <brian@espressif.com>
2020-05-11 00:37:17 +0800

    add card modal component

---
+++ src/components/CardModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card-modal">
+    <div class="modal-content">
+      <span class="close-button">&times;</span>
+      <h1>{{ card.title }}</h1>
+      <p>Assigned to: {{ card.assignee }}</p>
+      <p>Created by: {{ card.owner }}</p>
+      <p>{{ card.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { card } from "../board";
+
+@Component
+export default class CardModal extends Vue {
+  @Prop() card!: card;
+  // https://sabe.io/tutorials/how-to-create-modal-popup-box
+}
+</script>
