0a1c8d6c236aac1fd0b4f1573924c754d7e92843
brian@espressif.com <brian@espressif.com>
2020-05-13 00:54:12 +0800

    add modal css

--- src/components/CardModal.vue
+++ src/components/CardModal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="card-modal">
+  <div class="card-modal" :class="{ active: isActive }">
     <div class="modal-content">
-      <span class="close-button">&times;</span>
+      <span class="close-button" @click="toggleModal">&times;</span>
       <h1>{{ card.title }}</h1>
       <p>Assigned to: {{ card.assignee }}</p>
       <p>Created by: {{ card.owner }}</p>
@@ -18,6 +18,56 @@ import { card } from "../board";
 @Component
 export default class CardModal extends Vue {
   @Prop() card!: card;
+  @Prop() isActive: boolean;
   // https://sabe.io/tutorials/how-to-create-modal-popup-box
+  toggleModal() {
+    this.isActive = !this.isActive;
+  }
 }
 </script>
+
+<style scoped>
+.card-modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  visibility: hidden;
+  transform: scale(1.1);
+  transition: visibility 0s linear 0.25s, opacity 0.25s 0s, transform 0.25s;
+}
+
+.modal-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 1rem 1.5rem;
+  width: 24rem;
+  border-radius: 0.5em;
+}
+
+.close-button {
+  float: right;
+  width: 1.5rem;
+  line-height: 1.5rem;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 0.25rem;
+  background-color: lightgray;
+}
+.close-button {
+  background-color: darkgray;
+}
+
+.active {
+  opacity: 1;
+  visibility: visible;
+  transform: scale(1);
+  transition: visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s;
+}
+</style>
