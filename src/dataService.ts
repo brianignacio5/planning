3091f77abc80639f146f66e6c53bc3367943b071
brian@espressif.com <brian@espressif.com>
2020-07-30 19:38:57 +0800

    add card dataService logic

--- src/dataService.ts
+++ src/dataService.ts
@@ -1,10 +1,26 @@
 import http from "./http-common";
+import { card } from './board';
 
 class PlanningDataService {
   async loginUserByGithub() {
     const response = await http.get("http://localhost:3000/auth/github/session", {
       headers: { "Content-Type": "application/json" },
     });
+    console.log(response.data);
+    return response;
+  }
+  async createCard(newCard: card) {
+    const response = await http.post("http://localhost:3000/card", newCard);
+    console.log(response.data);
+    return response;
+  }
+  async updateCard(newCard: card) {
+    const response = await http.put(`http://localhost:3000/card/${newCard.id}`, newCard);
+    console.log(response.data);
+    return response;
+  }
+  async deleteCard(newCard: card) {
+    const response = await http.delete(`http://localhost:3000/card/${newCard.id}`);
     console.log(response.data);
     return response;
   }
